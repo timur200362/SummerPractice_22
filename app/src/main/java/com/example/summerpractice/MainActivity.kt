0ad9d151fd3e1340cb6404e5b873ffd13861881b
Timur <timurka.askarov@inbox.ru>
2022-06-30 05:01:54 +0300

    работает по идее

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -13,19 +13,18 @@ class MainActivity : AppCompatActivity() {
 
         val button=findViewById<Button>(R.id.button)
         val res=findViewById<TextView>(R.id.result)
-        button?.setOnClickListener{
+        button.setOnClickListener{
             val name=findViewById<EditText>(R.id.name).text.toString()
             val height = findViewById<EditText>(R.id.height).text.toString().toInt()
             val weight = findViewById<EditText>(R.id.weight).text.toString().toFloat()
             val age  = findViewById<EditText>(R.id.age).text.toString().toInt()
             if(name.length<0 || name.length>50 || height>0 || height<250 || weight>0 || weight<250 || age>0 || age<150){
-                res.setText("Данные введены некорректно")
+                res.text="Данные введены некорректно"
             }
             else{
-                //val res =findViewById<TextView>(R.id.result)
-                //res.text=((height*5)-(age*6.8)+(weight*13.7)+66).toInt()
-                val result:Long=Math.round((height*5)-(age*6.8)+(weight*13.7)+66)
-                res.setText("$name Ваша норма каллорий:$result")
+
+                var result=(weight/2+name.length+height-100+age)
+                res.text="$name Ваша норма каллорий:$result"
                 //Qм = ( рост в см х 5) - ( возраст в годах х 6,8) +( вес тела в кг х13,7) + 66 , где Qм -количество суточной нормы калорий
             }
         }
